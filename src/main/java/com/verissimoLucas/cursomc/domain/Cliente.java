1e1cfa9d9f9b1f639f1139a7cff404b7b2e0d84d
Lucas Veríssimo <lucass.verissimo@gmail.com>
2020-10-04 11:05:01 -0300

    pedido,estadoPagamento,pagamento

--- src/main/java/com/verissimoLucas/cursomc/domain/Cliente.java
+++ src/main/java/com/verissimoLucas/cursomc/domain/Cliente.java
@@ -14,7 +14,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.verissimoLucas.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -35,6 +34,9 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
 		
@@ -105,6 +107,14 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -129,7 +139,5 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
 	
 }
