64d2b11708687c454b70d0d910d2b21fe2139428
Lucas Veríssimo <lucass.verissimo@gmail.com>
2020-10-04 17:15:31 -0300

    delete categoria

--- src/main/java/com/verissimoLucas/cursomc/services/CategoriaService.java
+++ src/main/java/com/verissimoLucas/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.verissimoLucas.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.verissimoLucas.cursomc.domain.Categoria;
 import com.verissimoLucas.cursomc.repositories.CategoriaRepository;
+import com.verissimoLucas.cursomc.services.exceptions.DataIntegrityException;
 import com.verissimoLucas.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);					
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possua produtos");			
+		}
 	}
 
 }
