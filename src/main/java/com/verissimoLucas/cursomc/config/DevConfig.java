4b08705d887c94f125697eca9104a6d0d4642c23
Lucas Veríssimo <lucass.verissimo@gmail.com>
2020-10-25 22:03:18 -0300

    Envio de email smtp

--- src/main/java/com/verissimoLucas/cursomc/config/DevConfig.java
+++ src/main/java/com/verissimoLucas/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.verissimoLucas.cursomc.services.DBService;
+import com.verissimoLucas.cursomc.services.EmailService;
+import com.verissimoLucas.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -28,6 +30,11 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean 
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
--- src/main/java/com/verissimoLucas/cursomc/services/DBService.java
+++ src/main/java/com/verissimoLucas/cursomc/services/DBService.java
@@ -129,7 +129,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
-		Cliente cli1 = new Cliente(null,"Maria Silva","maria@gmail.com","14414152655",TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null,"Maria Silva","lucasverissimo1991@gmail.com","14414152655",TipoCliente.PESSOAFISICA);
 		
 		cli1.getTelefones().addAll(Arrays.asList("32356262","88794545"));			
 		
