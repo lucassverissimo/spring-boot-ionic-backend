5d1704d4d6f3e66af42ddb0159008419cfbc1bc4
Lucas Veríssimo <lucass.verissimo@gmail.com>
2020-10-27 17:44:54 -0300

    obtendo pedidos do cliente logado.

--- src/main/java/com/verissimoLucas/cursomc/services/PedidoService.java
+++ src/main/java/com/verissimoLucas/cursomc/services/PedidoService.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.verissimoLucas.cursomc.domain.Cliente;
 import com.verissimoLucas.cursomc.domain.ItemPedido;
 import com.verissimoLucas.cursomc.domain.PagamentoComBoleto;
 import com.verissimoLucas.cursomc.domain.Pedido;
@@ -15,6 +19,8 @@ import com.verissimoLucas.cursomc.domain.enums.EstadoPagamento;
 import com.verissimoLucas.cursomc.repositories.ItemPedidoRepository;
 import com.verissimoLucas.cursomc.repositories.PagamentoRepository;
 import com.verissimoLucas.cursomc.repositories.PedidoRepository;
+import com.verissimoLucas.cursomc.security.UserSS;
+import com.verissimoLucas.cursomc.services.exceptions.AuthorizationException;
 import com.verissimoLucas.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -70,6 +76,17 @@ public class PedidoService {
 		itemPedidoRespository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null){
+			throw new AuthorizationException("Acesso negado!!");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
