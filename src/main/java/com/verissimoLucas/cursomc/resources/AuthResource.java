7c91328dfc481ea126b0b83bae658b9d846b8830
Lucas Veríssimo <lucass.verissimo@gmail.com>
2020-10-29 07:07:33 -0300

    esqueci a senha

--- src/main/java/com/verissimoLucas/cursomc/resources/AuthResource.java
+++ src/main/java/com/verissimoLucas/cursomc/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.verissimoLucas.cursomc.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.verissimoLucas.cursomc.dto.EmailDTO;
 import com.verissimoLucas.cursomc.security.JWTUtil;
 import com.verissimoLucas.cursomc.security.UserSS;
+import com.verissimoLucas.cursomc.services.AuthService;
 import com.verissimoLucas.cursomc.services.UserService;
 
 @RestController
@@ -17,12 +21,22 @@ import com.verissimoLucas.cursomc.services.UserService;
 public class AuthResource {
 
 	@Autowired
-	JWTUtil jwtUtil;
+	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService service;
+	
 	@RequestMapping(value = "/refresh_token",method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response){
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Baerer "+token);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot",method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto){
+		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/verissimoLucas/cursomc/services/EmailService.java
+++ src/main/java/com/verissimoLucas/cursomc/services/EmailService.java
@@ -2,6 +2,7 @@ package com.verissimoLucas.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.verissimoLucas.cursomc.domain.Cliente;
 import com.verissimoLucas.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -9,5 +10,7 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 	
 }
